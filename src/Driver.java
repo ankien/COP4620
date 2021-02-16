108d414c874774d2b4b34bbc065e44e274b9c669
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2021-02-16 18:01:52 -0500

    Driver.java and Little.g4 for Step2

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,32 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.PrintWriter;
-import java.util.Scanner;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Driver {
     public static void main(String[] args) throws Exception {
-        /// Step 1: Scanner/Tokenizer/Lexer ///
-        // Read the LITTLE file
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        Little lexer = new Little(input);
+        /* Step 1: Scanner/Tokenizer/Lexer */
 
+        // create a CharStream that reads from standard input
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        // create a lexer that feeds off of input CharStream
+        LittleLexer lexer = new LittleLexer(input);
+
+        /*Step 2: Parser*/
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        LittleParser parser = new LittleParser(tokens);
+
+        parser.program(); // begin parsing at program rule
+        if( parser.getNumberOfSyntaxErrors() == 0 ){
+            System.out.println("Accepted");
+        } else {
+            System.out.println("Not accepted");
+        }
+
+        /*
         // Write to output file
         Token token = lexer.nextToken();
         while(token.getType() != Little.EOF) {
@@ -19,8 +34,9 @@ public class Driver {
                     "\nValue: " + token.getText());
             token = lexer.nextToken();
         }
+         */
     }
-
+/*
     // Step 1 Helper(s)
     private static String getTokenType(int tokenType) {
         switch(tokenType) {
@@ -40,4 +56,5 @@ public class Driver {
                 return "UNIDENTIFIED";
         }
     }
+ */
 }

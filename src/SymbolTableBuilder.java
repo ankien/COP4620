3b1759dc0d66d89d58cbc5995bf62b7f153ef741
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2021-03-15 11:23:07 -0400

    More changes still need to finish prettyPrint()

--- src/SymbolTableBuilder.java
+++ src/SymbolTableBuilder.java
@@ -3,16 +3,18 @@ import java.util.*;
 
 public class SymbolTableBuilder extends LittleBaseListener {
 
-	int blockNum = 1;
+	private int blockNum = 1;
 	private Stack<Hashtable<String, String>> currSymTab; //Stack of symbol tables. Current symbol table is on top of stack.
-	private Stack<Hashtable<String, String>> finishedSymTab; //popped symbol tables will go here
+	private Hashtable<String, Hashtable<String, String>> finishedSymTab; //popped symbol tables will go here
+	private int position = 0; //Position of nested functions or blocks. 0 is global symtab.
+	private int varPosition = 0; //Stores order in which variables were declared.
 	private Hashtable<String, String> temp;
 	String[] keys;
 
 	/*no arg constructor*/
 	public SymbolTableBuilder(){
 		currSymTab = new Stack<Hashtable<String, String>>();
-		finishedSymTab = new Stack<Hashtable<String, String>>();
+		finishedSymTab = new Hashtable<String,Hashtable<String, String>>();
 	}
 
 	/*Enter the Program*/
@@ -20,13 +22,17 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterProgram(LittleParser.ProgramContext ctx) {
 		Hashtable<String, String> global = new Hashtable<>();
 		global.put("*", "GLOBAL"); //key "*" in symbol table denotes context
+		global.put("#", String.valueOf(position)); //key "#" in symbol table denotes position of block/function. 0 is GLOBAL.
+		global.put("$", "0"); //Key "$" in symbol table denotes variable's position
+		position++;
 		currSymTab.push(global);
 	}
 
 	/*Exit program pop last symtab*/
 	@Override
 	public void exitProgram(LittleParser.ProgramContext ctx) {
-		finishedSymTab.push(currSymTab.pop());
+		temp = currSymTab.pop();
+		finishedSymTab.put(temp.remove("#"), temp);
 	}
 
 	/*Recognizes STRING*/
@@ -34,12 +40,13 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterString_decl(LittleParser.String_declContext ctx) {
 		temp = currSymTab.pop();
 		keys = (ctx.id().getText()).split(",");
-
+		varPosition = Integer.valueOf(temp.get("$"));
 		for( int i = 0; i < keys.length; i++ ) {
 			checkHashtable(temp, keys[i] );
-			temp.put( keys[i], "STRING " + ctx.str().getText() );
+			temp.put( keys[i], String.valueOf(varPosition) + " STRING " + ctx.str().getText() );
+			varPosition++;
 		}
-
+		temp.put("$", String.valueOf(varPosition));
 		currSymTab.push(temp);
 	}
 
@@ -48,12 +55,15 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterVar_decl(LittleParser.Var_declContext ctx) {
 		temp = currSymTab.pop();
 		keys = (ctx.id_list().getText()).split(",");
+		varPosition = Integer.valueOf(temp.get("$"));
 
 		for(int i = 0; i < keys.length; i++){
 			checkHashtable(temp, keys[i]);
-			temp.put(keys[i], ctx.var_type().getText());
+			temp.put(keys[i], String.valueOf(varPosition) + " " + ctx.var_type().getText());
+			varPosition++;
 		}
 
+		temp.put("$",String.valueOf(varPosition));
 		currSymTab.push(temp);
 	}
 
@@ -62,26 +72,33 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterFunc_decl(LittleParser.Func_declContext ctx) {
 		Hashtable<String, String> func = new Hashtable<>();
 		func.put("*", ctx.id().getText());
-		if( ctx.param_decl_list().param_decl() != null ) {
+		func.put("#", String.valueOf(position));
+		varPosition = 0;
+		position++;
 
+		//put function parameters into func hashtable
+		if( ctx.param_decl_list().param_decl() != null ) {
 			//Split function parameters INTx, INTy, FLOATx by ","
 			String[] parameters = (ctx.param_decl_list().getText()).split(",");
 
-			//Do a positive look behind to split strings preceded by INT or FLOAT
+			//Do a positive look behind to split function parameters preceded by INT or FLOAT
 			for(int i = 0; i < parameters.length; i++){
 				String type = (parameters[i].split("(?<=INT|FLOAT)"))[0]; //(this mess)[0] contains the type of parameter
  				String key = (parameters[i].split("(?<=INT|FLOAT)"))[1]; //(this mess)[1] contains the key for symtab
 				checkHashtable(func, key);
-				func.put(key, type);
+				func.put(key, String.valueOf(varPosition) + " " + type);
+				varPosition++;
 			}
 		}
+		func.put("$",String.valueOf(varPosition));
 		currSymTab.push(func);
 	}
 
 	//pop function scoped symbol table when exiting function
 	@Override
 	public void exitFunc_decl(LittleParser.Func_declContext ctx) {
-		finishedSymTab.push(currSymTab.pop());
+		temp = currSymTab.pop();
+		finishedSymTab.put(temp.remove("#"), temp);
 	}
 
 	//Enter if statement then create new symtab
@@ -89,6 +106,9 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterIf_stmt(LittleParser.If_stmtContext ctx) {
 		Hashtable<String, String> block = new Hashtable<>();
 		block.put("*", "BLOCK "+blockNum);
+		block.put("#", String.valueOf(position));
+		block.put("$", "0");
+		position++;
 		blockNum++;
 		currSymTab.push(block);
 	}
@@ -96,7 +116,8 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	//Exit if statement. Pop currsymtab
 	@Override
 	public void exitIf_stmt(LittleParser.If_stmtContext ctx) {
-		finishedSymTab.push(currSymTab.pop());
+		temp = currSymTab.pop();
+		finishedSymTab.put(temp.remove("#"), temp);
 	}
 
 	/*Enter ELSE statement create new symtab.*/
@@ -104,6 +125,9 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterElse_part(LittleParser.Else_partContext ctx) {
 		Hashtable<String, String> block = new Hashtable<>();
 		block.put("*", "BLOCK "+blockNum);
+		block.put("#", String.valueOf(position));
+		block.put("$", "0");
+		position++;
 		blockNum++;
 		currSymTab.push(block);
 	}
@@ -111,7 +135,8 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	/*Exit ELSE statement. Pop symtab.*/
 	@Override
 	public void exitElse_part(LittleParser.Else_partContext ctx) {
-		finishedSymTab.push(currSymTab.pop());
+		temp = currSymTab.pop();
+		finishedSymTab.put(temp.remove("#"), temp);
 	}
 
 	//Enter while statement then create new symtab
@@ -119,6 +144,9 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	public void enterWhile_stmt(LittleParser.While_stmtContext ctx) {
 		Hashtable<String, String> block = new Hashtable<>();
 		block.put("*", "BLOCK "+blockNum);
+		block.put("#", String.valueOf(position));
+		block.put("$", "0");
+		position++;
 		blockNum++;
 		currSymTab.push(block);
 	}
@@ -126,11 +154,16 @@ public class SymbolTableBuilder extends LittleBaseListener {
 	//Exit while statement. Pop currsymtab
 	@Override
 	public void exitWhile_stmt(LittleParser.While_stmtContext ctx) {
-		finishedSymTab.push(currSymTab.pop());
+		temp = currSymTab.pop();
+		finishedSymTab.put(temp.remove("#"), temp);
 	}
 
 	public void prettyPrint(){
-		System.out.println(finishedSymTab);
+		for(int i = 0; i < position; i++){
+			temp = finishedSymTab.get(String.valueOf(i));
+			System.out.println(temp);
+			System.out.println();
+		}
 	}
 
 	//check if variable name got declared twice in same scope
